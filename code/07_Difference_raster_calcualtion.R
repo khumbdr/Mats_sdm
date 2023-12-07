08d88b77a6167130482a8f57a6d405ae22cf0276
khumbdr <khumbdr@gmail.com>
2023-12-07 15:35:49 -0700

    editing texts

--- code/07_Difference_raster_calcualtion.R
+++ code/07_Difference_raster_calcualtion.R
@@ -15,11 +15,11 @@ diff<-sdm-unmix
 # converting into data frame
 diff.df<-as.data.frame(diff,xy=TRUE)
 
-# callating number of pixel in each classfication
+# callating number of pixel in each classification
 table(diff.df$layer)
 # where,
-# 2-1=1,presence of blackmats in both sdm and unmix raster
-# 2-0=2, presence of blackmats only in sdm raster
-# 0-1=-1, absence of blackmats in sdm but presence in unmix raster
-# 0-0=0, absence of blackmats in sdm and also absence in unmix raster
+# 2-1=1,presence of black mats in both sdm and unmix raster
+# 2-0=2, presence of black mats only in sdm raster
+# 0-1=-1, absence of black mats in sdm but presence in unmix raster
+# 0-0=0, absence of black mats in sdm and also absence in unmix raster
 
